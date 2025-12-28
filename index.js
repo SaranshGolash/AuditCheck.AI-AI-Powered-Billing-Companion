804fe187d924fb92b61c0b900fada02be6f7fa7b
SaranshGolash <129923993+SaranshGolash@users.noreply.github.com>
2025-12-28 11:04:36 +0530

    Ready for Deployment

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ const pool = new Pool({
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT || 5432,
+    ssl: isProduction ? { rejectUnauthorized: false } : false
 });
 
 const rawData = fs.readFileSync(path.join(__dirname, 'data', 'healthcare_pricing.json'));
